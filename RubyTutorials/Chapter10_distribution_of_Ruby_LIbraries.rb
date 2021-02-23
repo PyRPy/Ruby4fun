33e346c1bf077038bcbf9c448f04a8b95aa4d94f
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-02-23 11:05:18 -0500

    distribution : gem

---
+++ RubyTutorials/Chapter10_distribution_of_Ruby_LIbraries.rb
@@ -0,0 +1,20 @@
+# Chapter 10 Distributing Ruby Code and Libraries
+
+# Accessing Command-Line Arguments
+
+# Distributing Ruby Libraries As Gems
+# command lines for building gems
+# > gem build string_extend.gemspec
+# > gem install string_extend
+
+# to use it like this :
+# > require 'string_extend'
+
+# Easier Gem Creation
+# > bundle gem string_extend
+
+# where to find more gems
+# RubyGems.org
+
+# CGI Scripts
+# Common Gateway Interface (CGI)
