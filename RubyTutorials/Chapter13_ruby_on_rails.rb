60dff6c2f38eeca40949fae067721f269cf194b6
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-02-24 12:33:03 -0500

    how to build a webapp using ruby on rails

---
+++ RubyTutorials/Chapter13_ruby_on_rails.rb
@@ -0,0 +1,14 @@
+# Two Web Application Approaches: Rails and Sinatra
+# Outline only for this chapter
+
+# Installing Rails
+# Database Considerations
+# Creating a Blank Rails Application
+# The Rails Command-Line Tool
+# Files and Directories Within a Rails Application
+# Database Configuration
+# Using Scaffolding
+# Database Migrations
+
+# Running the Basic, Scaffolded App
+# Controllers and Views
